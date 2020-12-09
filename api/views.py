f30866d8743abbc964564cb0b6e9fc811b2403ed
PazBazak <yolobazak1@gmail.com>
2020-12-09 12:26:53 +0200

    handle read message

--- api/views.py
+++ api/views.py
@@ -12,7 +12,17 @@ from rest_framework.authtoken.models import Token
 from django.db.models import Q
 
 
-# Create your views here.
+# region utils
+
+def filter_sender_receiver(username):
+    """
+    Returns the queryset filter for message if the username is in either sender or receiver
+    :param username: username
+    :return: filter queryset
+    """
+    return Q(sender__username=username) | Q(receiver__username=username)
+
+# endregion
 
 
 class MessageViewSet(viewsets.ModelViewSet):
@@ -21,6 +31,8 @@ class MessageViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action == CREATE:
             return MessageCreateSerializer
+        if self.action == RETRIEVE:
+            return MessageFullDisplaySerializer
         return MessageDisplaySerializer
 
     def destroy(self, request, *args, **kwargs):
@@ -37,6 +49,32 @@ class MessageViewSet(viewsets.ModelViewSet):
                 return Response(status=status.HTTP_204_NO_CONTENT)
 
         return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+        When fetching single message, it will return the message only if the logged user is eiter the sender or the
+        receiver, and if the receiver fetched the message, it will be changed to read!
+        """
+        request_token = request.auth.key
+        token = Token.objects.get(key=request_token)
+
+        user = token.user
+        logged_username = user.username
+
+        instance = self.get_object()
+        instance_sender = instance.sender
+        instance_receiver = instance.receiver
+
+        # if the logged user is matching either the sender or receiver
+        if logged_username == instance_sender.username or logged_username == instance_receiver.username:
+            if not instance.is_read:
+                if logged_username == instance_receiver.username:
+                    instance.is_read = True
+                    instance.save()
+            serializer = self.get_serializer(instance)
+            return Response(serializer.data)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -60,15 +98,6 @@ class UserViewSet(viewsets.ModelViewSet):
     # use register for creating new instances
     def create(self, request, *args, **kwargs):
         return Response(status=status.HTTP_403_FORBIDDEN)
-
-    @staticmethod
-    def filter_sender_receiver(username):
-        """
-        Returns the queryset filter for message if the username is in either sender or receiver
-        :param username: username
-        :return: filter queryset
-        """
-        return Q(sender__username=username) | Q(receiver__username=username)
 
     @action(detail=False, methods=['post'])
     def register(self, request, *args, **kwargs):
@@ -103,7 +132,7 @@ class UserViewSet(viewsets.ModelViewSet):
 
         username = user.username
 
-        messages_queryset = Message.objects.filter(self.filter_sender_receiver(username))
+        messages_queryset = Message.objects.filter(filter_sender_receiver(username))
         messages = MessageFullDisplaySerializer(messages_queryset, many=True)
 
         return Response(messages.data)
@@ -121,7 +150,7 @@ class UserViewSet(viewsets.ModelViewSet):
             return Response(status=status.HTTP_401_UNAUTHORIZED)
 
         username = user.username
-        messages_queryset = Message.objects.filter(self.filter_sender_receiver(username)).filter(is_read=False)
+        messages_queryset = Message.objects.filter(filter_sender_receiver(username)).filter(is_read=False)
         messages = MessageFullDisplaySerializer(messages_queryset, many=True)
 
         return Response(messages.data)
